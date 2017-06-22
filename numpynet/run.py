eeece26f39054c1d4b531909a55b43f7677df547
Juho Kilkki <juho.kilkki@gmail.com>
2017-06-22 15:07:32 +0300

    working xor test

--- numpynet/run.py
+++ numpynet/run.py
@@ -1,1 +1,25 @@
-# The net and everything
+
+from data import Data
+from net import Net
+from gradient_descent import Descender
+
+if __name__=='__main__':
+
+    # timeit descend()
+
+    xordata = Data()
+    net = Net([5], xordata)
+    desc = Descender(net)
+    
+    # idx, sqes = zip(*desc.descend(100))
+
+    idx, sqes = [], []
+    for i, s in desc.descend(100):
+    	print(i, s)
+    	idx.append(i)
+    	sqes.append(s)
+
+    # from pylab import *
+    # ion()
+    # plot(idx, sqes)
+    # ylim([0, sqes[0]*1.05])
