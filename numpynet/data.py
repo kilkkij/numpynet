c73ae7a5f5b6855a270af1a63ca9342a7223797e
Juho Kilkki <juho.kilkki@gmail.com>
2017-06-22 15:09:11 +0300

    working xor test

---
+++ numpynet/data.py
@@ -0,0 +1,17 @@
+
+from numpy import asarray
+
+obs = [([0.0, 0.0], [-1.]), ([0.0, 1.0], [1.0]), ([1.0, 0.0], [1.0]), ([1.0, 1.0], [-1.]),]
+obs = obs*100000
+BATCHSIZE = len(obs)
+INSIZE = 2
+OUTSIZE = 1
+OBSERVATIONS = asarray([yo for yi, yo in obs]).reshape((BATCHSIZE, 1, 1))
+
+class Data:
+	def __init__(self):
+		self.values = OBSERVATIONS
+		self.batchsize = len(obs)
+		self.insize = 2
+		self.outsize = 1
+		self.obs = obs
---
+++ numpynet/gradient_descent.py
@@ -0,0 +1,39 @@
+
+import numpy as np
+
+LRATE = .2
+MOMENTUM = .4
+
+class Descender(object):
+
+    """Gradient descent."""
+
+    def __init__(self, net):
+        self.net = net
+        self.delta_prev = [np.copy(dWi) for dWi in net.dW]
+
+    def descend_batch(self):
+        W = self.net.W
+        ax = self.net.ax
+        # Activate net
+        self.net.activate()
+        # Energy gradient matrices
+        dEdW = self.net.weight_gradient()
+        # Update weights
+        for wi in range(self.net.depth-1):
+            # Weight increment. Doesn't include momentum
+            delta_wi = -LRATE*dEdW[wi]
+            # Descend step
+            W[wi][:] += delta_wi + MOMENTUM*self.delta_prev[wi]
+            # Save weight increment for the next step
+            self.delta_prev[wi][:] = delta_wi
+        # Squared errors
+        sqerrors = (self.net.data.values - ax[-1])**2
+        return sqerrors
+
+    def descend(self, N):
+        for i in range(N):
+            chunk_errors = [self.descend_batch() for _ in range(1)]
+            sqerror = np.mean(chunk_errors)
+            # print(i, sqerror)
+            yield i, sqerror
